1fff2c2bd229c6101c6af7dd585252f5c10ee46b
Vivi <jiahui1001@yeah.net>
2021-03-24 15:22:49 +0800

    改造Notes.vue 使其变为通用组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,40 +1,45 @@
 <template>
   <div>
     <label class="notes">
-      <span class="name">备注</span>
+      <span class="name">{{ this.fieldName }}</span>
       <input type="text"
              v-model="value"
-             placeholder="在这里输入备注">
+             :placeholder=this.placeholder>
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
   value = '';
+
+  @Prop({required: true}) fieldName!: string;
+  @Prop() placeholder?: string;
+
   @Watch('value')
-    onValueChange(value: string){
-      this.$emit('update:value',value)
-    }
+  onValueChange(value: string) {
+    this.$emit('update:value', value);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.notes{
+.notes {
   font-size: 14px;
   display: flex;
   align-items: center;
   background: #E9FDE4;
-  padding-left: 16px ;
+  padding-left: 16px;
 
-  .name{
+  .name {
     padding-right: 16px;
   }
-  input{
+
+  input {
     height: 62px;
     flex-grow: 1;
     background: transparent;
